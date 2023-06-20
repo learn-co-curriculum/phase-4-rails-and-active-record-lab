3b95740aad055f63ce076cd2f741f5ea0990bca1
Meshack Korir <meshackkorir100@gmail.com>
2023-06-20 10:16:56 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: "Dwayne", last_name: "Johnson", grade: 7)
